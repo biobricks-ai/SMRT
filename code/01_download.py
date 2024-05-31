47aed30c2936565fd2f153948a48e61fedafa98d
Thomas Luechtefeld <tom@insilica.co>
2024-05-31 21:14:34 +0000

    finished version complete

--- code/01_download.py
+++ code/01_download.py
@@ -5,3 +5,12 @@ import os
 os.makedirs('download', exist_ok=True)
 
 # read the data from the ./download directory
+import requests
+source_url = "https://figshare.com/ndownloader/articles/8038913/versions/1"
+
+# stream the source_url to the ./download/smrt_data.zip
+with requests.get(source_url, stream=True) as r:
+    r.raise_for_status()
+    with open('download/smrt_data.zip', 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
--- code/02_process.py
+++ code/02_process.py
@@ -1,7 +1,20 @@
 # PURPOSE: CHANGE THE DOWNLOADED DATA TO ONE OR MORE PARQUET FILES
-import os 
+import os, shutil, zipfile, pandas as pd
 
 # exports to the ./brick directory
 os.makedirs('brick', exist_ok=True)
 
-# read the data from the ./download directory
+# make a temp directory
+os.makedirs('temp', exist_ok=True)
+
+# unzip the download/smrt_data.zip to temp directory
+with zipfile.ZipFile('download/smrt_data.zip', 'r') as zip_ref:
+    zip_ref.extractall('temp')
+
+# transform SMRT_DATA to PARQUET
+smrt_datset = pd.read_csv('temp/SMRT_dataset.csv',sep=';')
+smrt_datset.to_parquet('brick/smrt_dataset.parquet')
+
+# skip other assets which are all generated or redundant to this raw data
+# remove temp directory
+shutil.rmtree('temp')
